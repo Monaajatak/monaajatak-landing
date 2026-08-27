ccb46724b969a1f8b1e7c88aab16242b14d34b6c
mohamed_ali <mohamedalione11@gmail.com>
2026-08-27 18:45:05 +0300

    feat(tailwind): add Tailwind and color system configuration and add thmanyah-font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
   
   runtimeConfig: {
     googlePlayServiceAccountJson: process.env.GOOGLE_PLAY_SERVICE_ACCOUNT_JSON,
